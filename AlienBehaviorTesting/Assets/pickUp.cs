99c9cc4a5ea9fa02fb34eb701268369e51083932
HunterOka <okah1@nku.edu>
2015-02-19 15:28:48 -0500

    add cow

---
+++ AlienBehaviorTesting/Assets/pickUp.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pickUp : MonoBehaviour {
+
+	private GameObject cow;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	
+		if (Input.GetAxis("Vertical") != 0)
+		{
+			if (cow != null)
+			{
+				cow.hingeJoint.breakForce = .001f;
+				cow.AddComponent("SphereCollider") as SphereCollider;
+				cow.AddComponent("HingeJoint") as HingeJoint;
+				cow= null;
+			}
+			
+		}
+	
+	}
+	
+	void OnCollisionEnter(Collision c)
+	{
+		if (c.gameObject.CompareTag("Livestock"))
+		{
+			cow = c.gameObject;
+			c.gameObject.hingeJoint.connectedBody = rigidbody;
+		}
+		
+		
+		
+		
+	
+	}
+	
+}
