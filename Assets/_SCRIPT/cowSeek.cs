4bc9b240b9ff99d1c857ecd879823bdc5a930ee4
HunterOka <okah1@nku073089.hh.nku.edu>
2015-02-25 19:32:48 -0500

    claw ship tweaks, add FlightArea prefab

--- Assets/_SCRIPT/cowSeek.cs
+++ Assets/_SCRIPT/cowSeek.cs
@@ -2,7 +2,9 @@
 using System.Collections;
 
 public class cowSeek : MonoBehaviour {
-	
+
+	private float speed = 10f;
+	private float wiggle = 5f;
 	private GameObject[] livestock;
 	private GameObject target;
 	private float hoverHeight = 12;
@@ -23,8 +25,14 @@ public class cowSeek : MonoBehaviour {
 
 	if (target == null)
 	{
-
+			if (livestock.Length != 0) {
+			
 			acquireTarget(livestock[Random.Range(0, livestock.Length)]);
+			}
+			else
+			{
+				acquireTarget(portal);
+			}
 	
 	}
 	
@@ -35,10 +43,32 @@ public class cowSeek : MonoBehaviour {
 	
 	}
 	
+	overTarget = new Vector3( target.transform.position.x, target.transform.position.y + hoverHeight,  target.transform.position.z);
+
 	if (target != null){
 		
-		transform.position = Vector3.Lerp(transform.position, overTarget, Time.deltaTime);
-		overTarget = new Vector3( target.transform.position.x, target.transform.position.y + hoverHeight,  target.transform.position.z);
+		
+		//transform.position = Vector3.Lerp(transform.position, , Time.deltaTime);
+
+
+			float distance=Vector3.Distance(transform.position, overTarget);
+			if(distance>0.2){
+
+
+				transform.position = Vector3.Lerp (
+					transform.position, overTarget,
+					Time.deltaTime* speed/distance);
+			}
+
+			if(distance <= 0.2){
+
+				overTarget = new Vector3( target.transform.position.x + Random.Range(-wiggle,wiggle), target.transform.position.y + hoverHeight  + Random.Range(-wiggle,wiggle),  target.transform.position.z  + Random.Range(-wiggle,wiggle));
+			}
+
+
+		
+		
+		
 	
 		}
 		
@@ -48,8 +78,12 @@ public class cowSeek : MonoBehaviour {
 		claw.SendMessage("releaseClaw");
 		
 		target = null;
+		
 		livestock = GameObject.FindGameObjectsWithTag("Livestock");
+
 		}
+
+
 	}
 	
 	void acquireTarget(GameObject g)
