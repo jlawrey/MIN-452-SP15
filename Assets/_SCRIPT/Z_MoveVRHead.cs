cc4e75938c96873aa5c0794fb52c2de975c6bfe5
Nicholas Zeman <nbzeman@gmail.com>
2015-02-25 19:48:28 -0500

    - updated head track stuff

---
+++ Assets/_SCRIPT/Z_MoveVRHead.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Z_MoveVRHead : MonoBehaviour {
+
+	public Transform VRHead;
+	public float mult;
+	public enum InputMethod {Mouse,Wii,Face};
+	public InputMethod HeadInput;
+	private float x;
+	private float y;
+	private float z;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetMouseButton(0) && HeadInput == InputMethod.Mouse) {
+			Vector3 headmover = new Vector3(Input.GetAxis ("Horizontal") * mult, Input.GetAxis("Vertical")*mult, VRHead.localPosition.z);
+			VRHead.localPosition = headmover;
+		}
+	}
+}
