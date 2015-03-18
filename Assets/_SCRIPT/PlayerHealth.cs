a2062583b5b6a0aa51856b7d04cb2ab7b7634512
jlawrey <j_lawrey@hotmail.com>
2015-03-18 19:47:02 -0400

    Added health hud and scripts
    Added health hud and scripts

---
+++ Assets/_SCRIPT/PlayerHealth.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour
+{
+	//public int startingHunger = 100;
+    public int startingHealth = 100;
+    public int currentHealth;
+	//public int currentHunger;
+	//public Slider hungerSlider;
+    public Slider healthSlider;
+    public Image damageImage;
+    public AudioClip deathClip;
+    public float flashSpeed = 5f;
+    public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
+
+
+    Animator anim;
+    AudioSource playerAudio;
+    //PlayerMovement playerMovement;
+    //PlayerShooting playerShooting;
+    bool isDead;
+    bool damaged;
+
+
+    void Awake ()
+    {
+        anim = GetComponent <Animator> ();
+        playerAudio = GetComponent <AudioSource> ();
+        //playerMovement = GetComponent <PlayerMovement> ();
+       //playerShooting = GetComponentInChildren <PlayerShooting> ();
+        currentHealth = startingHealth;
+		//currentHunger = startingHunger;
+    }
+
+
+    void Update ()
+    {
+        if(damaged)
+        {
+            damageImage.color = flashColour;
+        }
+        else
+        {
+            damageImage.color = Color.Lerp (damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
+        damaged = false;
+    }
+
+
+    public void TakeDamage (int amount)
+    {
+        damaged = true;
+
+        currentHealth -= amount;
+		//currentHunger -= amount;
+        healthSlider.value = currentHealth;
+		//hungerSlider.value = currentHunger;
+        playerAudio.Play ();
+
+        if(currentHealth <= 0 && !isDead)
+        {
+            Death ();
+        }
+    }
+
+
+    void Death ()
+    {
+        isDead = true;
+
+        //playerShooting.DisableEffects ();
+
+        anim.SetTrigger ("Die");
+
+        playerAudio.clip = deathClip;
+        playerAudio.Play ();
+
+        //playerMovement.enabled = false;
+        //playerShooting.enabled = false;
+    }
+}
