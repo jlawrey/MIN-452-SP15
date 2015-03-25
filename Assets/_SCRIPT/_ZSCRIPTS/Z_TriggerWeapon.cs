1761e51c71c052e150520973446008f2327ba4e1
Nicholas Zeman <nbzeman@gmail.com>
2015-03-25 18:00:02 -0400

    - adding some shield actions

--- Assets/_SCRIPT/_ZSCRIPTS/Z_TriggerWeapon.cs
+++ Assets/_SCRIPT/_ZSCRIPTS/Z_TriggerWeapon.cs
@@ -8,6 +8,7 @@ public class Z_TriggerWeapon : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
+		Wii.DeactivateMotionPlus (1);
 		anim = GetComponentInChildren<Animator> ();
 	}
 	
@@ -23,20 +24,23 @@ public class Z_TriggerWeapon : MonoBehaviour {
 
 		//bool ShieldClosed;
 
-		if (Input.GetKeyDown ("1") || Wii.IsPitchFast(1) && anim.GetCurrentAnimatorStateInfo(0).IsName("Idle") ) {
-			anim.SetTrigger ("Swing 1");
+		if (Input.GetKeyDown ("2") || Wii.IsPitchFast(1) && anim.GetCurrentAnimatorStateInfo(0).IsName("Idle") ) {
+			anim.SetTrigger ("Swing 2");
 		}
-		if(Input.GetKeyDown("2") ){
-			anim.SetTrigger("Swing 2");
+		if(Input.GetKeyDown("1") ){
+			anim.SetTrigger("Swing 1");
 		}
 		if(Input.GetKeyDown("3") || Wii.IsYawFast(1) && !Wii.IsPitchFast(1) && anim.GetCurrentAnimatorStateInfo(0).IsName("Idle") ){
 			anim.SetTrigger("Swing 3");
 		}
-		if(Input.GetKey("space") || Wii.GetNunchuckButton(1,"Z")){
-			anim.SetTrigger ("Close Shield");
+		if(Input.GetKeyDown("space") || Wii.GetButtonDown(1,"A")){
+			print ("SHIELD!!!");
+			if (!anim.GetBool("Close Shield")){
+				anim.SetBool("Close Shield",true);
+			}else{
+				anim.SetBool("Close Shield",false);
+			}
 		}
-		if(Input.GetKeyUp("space")|| Wii.GetNunchuckButtonUp(1,"Z")){
-			anim.SetTrigger ("Open Shield");
-		}
+
 	}
 }
