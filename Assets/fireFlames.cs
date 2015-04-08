503240ed7d0f6b46621f85b2c00b221ecb58ee85
HunterOka <okah1@nku073089.hh.nku.edu>
2015-04-08 19:35:53 -0400

    start of flameShip

---
+++ Assets/fireFlames.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fireFlames : MonoBehaviour {
+
+	float fireRate = 8;
+	float fireTimer;
+	float waittime = 3f;
+	public GameObject flame;
+	ParticleRenderer[] renderers;
+	// Use this for initialization
+	void Start () {
+		fireTimer = 3;
+		renderers = flame.GetComponentsInChildren<ParticleRenderer> ();
+		for (int i= 0; i < renderers.Length; i++) {
+			renderers [i].enabled = false;
+		}
+
+		transform.LookAt (GameObject.FindGameObjectWithTag("Player").transform);
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		fireTimer -= Time.deltaTime;
+		if (fireTimer <= 0)
+		{
+			StartCoroutine("fireFlamethrower");
+			Quaternion newRotation = Quaternion.AngleAxis(90, Vector3.up);
+			transform.rotation= Quaternion.Slerp(transform.rotation, newRotation, .05f);  
+			fireTimer = fireRate;
+
+
+
+		}
+	
+	}
+
+	public IEnumerator fireFlamethrower()
+	{
+		transform.Rotate(-90,0,0);
+
+
+		for (int i= 0; i < renderers.Length; i++) {
+			renderers [i].enabled = true;
+		}
+
+		yield return new WaitForSeconds (waittime);
+
+		for (int i= 0; i < renderers.Length; i++) {
+			renderers [i].enabled = false;
+		}
+
+		transform.Rotate(90,0,0);
+	}
+
+
+
+	
+}
---
+++ Assets/moveVertical.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveVertical : MonoBehaviour {
+
+
+	bool goUp = false;
+	Transform player ;
+	float interval = 4;
+	float verticalSpeed = 0.05f;
+
+	// Use this for initialization
+	void Start () {
+
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		if (transform.position.y  >  player.position.y + interval)
+		{ goUp = false;}
+		if (transform.position.y  <  player.position.y - interval)
+		{ goUp = true;}
+		
+		
+		if(goUp ){
+			
+			transform.Translate(0,verticalSpeed,0,Space.World);
+		}else  {
+			transform.Translate(0,-verticalSpeed,0,Space.World);
+		}
+	
+	}
+}
