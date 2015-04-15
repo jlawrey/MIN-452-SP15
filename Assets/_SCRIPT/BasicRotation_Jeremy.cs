3ddee16a90815c3b90746048ccd364061e950a2e
Jeremy Martin <jeremymartin1391@hotmail.com>
2015-04-15 06:48:39 -0400

    Updated Death Screen
    Visually updated death screen, still need to link the replay button.

---
+++ Assets/_SCRIPT/BasicRotation_Jeremy.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicRotation_Jeremy : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		transform.Rotate(Vector3.right, Time.deltaTime * speed);
+		transform.Rotate(Vector3.up, Time.deltaTime, Space.World);
+	}
+}
