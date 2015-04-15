4ea5fac38aaa856f43f324be709ee0de46e07345
nbzeman@gmail.com <nbzeman@gmail.com>
2015-04-15 19:50:58 -0400

    - helmet chucks at cam in death screen

---
+++ Assets/_SCRIPT/_ZSCRIPTS/Z_ChuckHelmet.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Z_ChuckHelmet : MonoBehaviour {
+
+	public int force;
+	public int torque;
+
+	// Use this for initialization
+	void Start () {
+	
+		this.gameObject.rigidbody.AddForce (0, 0, force);
+		this.gameObject.rigidbody.AddTorque (torque, 0, 0);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
