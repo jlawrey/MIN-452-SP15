7117de80f68b09fb0f8605456fc0a3b3f997a47c
jlawrey <j_lawrey@hotmail.com>
2015-04-14 21:20:57 -0400

    Added working menu
    Still needs an exit button and screens for options and instructions but
    buttons still work and take you to blank scenes.

---
+++ Assets/_SCRIPT/MenuScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+	
+	public void PlayGame(){
+		Application.LoadLevel("MMM_Level_01");
+	}
+
+	public void Options(){
+		Application.LoadLevel("Options");
+	}
+
+	public void Instructions(){
+		Application.LoadLevel("Instructions");
+	}
+
+	public void QuitGame(){
+		Application.Quit ();
+	}
+}
