6288f86629d8e119b166eee2396321e92c6a8a9d
Nicholas Zeman <nbzeman@gmail.com>
2015-04-21 11:34:44 -0400

    - trying to debug why the cursor stays up when the next application is loaded

--- Assets/_SCRIPT/_ZSCRIPTS/Z_KinectMouseClick.cs
+++ Assets/_SCRIPT/_ZSCRIPTS/Z_KinectMouseClick.cs
@@ -5,28 +5,29 @@ public class Z_KinectMouseClick : MonoBehaviour {
 
 	private InteractionManager manager;
 	public int level;
+	private bool kinect_be_on = true;
 
 	// Use this for initialization
 	void Start () {
 	
-
+		kinect_be_on = true;
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		if(manager == null)
-		{
-			manager = InteractionManager.Instance;
+		if(kinect_be_on){
+			if(manager == null){
+				manager = InteractionManager.Instance;
+			}
 		}
-
 	}
 
 	void OnMouseOver(){
 
 		if (manager.GetRightHandEvent() == InteractionWrapper.InteractionHandEventType.Grip) {
-			manager = null;
+			kinect_be_on = false;
 			print ("PlayMode!!");
 			Application.LoadLevel(level);
 		}
